dcd2f94e10a7d46572e11c81fb445495283942dd
Christof Weickhardt <38954618+somehowchris@users.noreply.github.com>
2019-01-13 14:54:42 +0100

    Adding adressverwaltungsform

--- src/adressverwaltung/Main.java
+++ src/adressverwaltung/Main.java
@@ -2,7 +2,16 @@ package adressverwaltung;
 
 /**
  *
- * @author Christof Weickhardt, Nicoal Temporal
+ * @author Christof Weickhardt
+ * @author Nicola Temporal
  */
 public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AdressverwaltungForm form = new AdressverwaltungForm();
+        form.setVisible(true);
+    }
 }
